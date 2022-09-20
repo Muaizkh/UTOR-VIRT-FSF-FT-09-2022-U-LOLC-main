cc701edcce5b29c993a87e5e6b6452ccace79813
Muaizkh <khanmu@my.yorku.ca>
2022-09-20 15:08:37 -0400

    updates made to challenge 03

--- 04-Web-APIs/02-Challenge/Assets/script.js
+++ 04-Web-APIs/02-Challenge/Assets/script.js
@@ -1,1 +1,17 @@
 var body = document.body; 
+var h1El = document.createElement("h1");
+var imgEl =document.createElement ("img");
+var infoEl = document.createElement("div");
+var quizEl = document.createElement("div");
+
+body.appendChild(h1El);
+body.appendChild(infoEl);
+body.appendChild(quizEl);
+
+h1El.textContent = "Coding Quiz Time!";
+infoEl.textContent = "The following timed quiz is to test your knowledge of HTML, CSS, and JavaScript so far.";
+
+h1El.setAttribute("style", "margin:auto; width:50%; text-align:center;");
+imgEl.setAttribute("src", "")
+infoEl.setAttribute("style", "margin:auto; width:50%; text-align:center;");
+
