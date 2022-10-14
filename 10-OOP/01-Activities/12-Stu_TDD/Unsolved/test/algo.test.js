2c2c2c2f28e259eb23d27b36b2fb10a4b5d1a3d7
Muaizkh <khanmu@my.yorku.ca>
2022-10-14 17:51:32 -0400

    added jest to 14 in 01

--- 10-OOP/01-Activities/12-Stu_TDD/Unsolved/test/algo.test.js
+++ 10-OOP/01-Activities/12-Stu_TDD/Unsolved/test/algo.test.js
@@ -1,15 +1,36 @@
 const Algo = require("../algo");
 
 describe("Algo", () => {
+  it("Should return an Algo object", () => {
+    const obj = new Algo();
+    expect(obj instanceof Algo).toEqual(true);
+  });
   describe("reverse", () => {
     // TODO: Write a test for the `reverse` method that should take a string as an argument and return a new reversed version of the string
+it("Should take a string as an argument and return the reverse version of the string", () =>{
+  const word= "olleH"
+  const reversed = new Algo().reverse("Hello");
+  expect(word === reversed).toEqual(true);
+});
   });
 
   describe("isPalindrome", () => {
     // TODO: Write a test for the `isPalindrome` method that should take a string as an argument and return the boolean `true` if the provided string is a palindrome.
+    it("Should take a string as an argument and return the boolean 'true' if the provided string is a palindrome", ()=> {
+      const palindrome1 = new Algo().isPalindrome("racecar");
+      const palindrome2 = new Algo().isPalindrome("neon");
+      expect(palindrome1).toEqual(true);
+      expect (palindrome2).toEqual(false);
+    });
   });
 
   describe("capitalize", () => {
     // TODO: Write a test for the `capitalize` method that should take a string as an argument and return a new string with the first letter of each word capitalized
+    it("Should take a string as an argument and return a new string with the first letter of each word capitalized", ()=>{
+      const capitalize = new Algo().capitalize(
+        "capotalize every first word of the string."
+      );
+      expect(capitalize).toEqual("Capatilize Every First Word Of The String.")
+    });
   });
 });
--- 10-OOP/01-Activities/14-Stu_Pass-Tests/Unsolved/algo.js
+++ 10-OOP/01-Activities/14-Stu_Pass-Tests/Unsolved/algo.js
@@ -2,14 +2,30 @@ function Algo() {}
 
 Algo.prototype.reverse = function(str) {
   // TODO: Your code here
+  let newString = "";
+  for(let i = str.length -1; i>=0; i --){
+    newString += str[i];
+  }
+return newString;
 };
 
 Algo.prototype.isPalindrome = function(str) {
   // TODO: Your code here
+  let array1 = [];
+  for(let i =0; i< str.length; i++){
+    array1.push(str[str.length - i -1]);
+  }
+  return (array1.join('') === str);
 };
 
 Algo.prototype.capitalize = function(str) {
   // TODO: Your code here 
+  return (str.replace(/^(.)/, function(b) {
+    return b.toUpperCase();
+  }) .replace(/\s(.)/g, function(a){
+    return a.toUpperCase();
+  }))
+    
 };
 
 module.exports = Algo;
